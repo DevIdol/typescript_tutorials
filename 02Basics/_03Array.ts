c3fcbb2dd9d7f0b1e5191ca320fa227d5b3d7f2f
DevIdol <devidol.mm@gmail.com>
2022-12-28 12:41:24 +0630

    array

---
+++ 02Basics/_03Array.ts
@@ -0,0 +1,18 @@
+const superHeros: string[] = []
+const heroPower: Array<number> = []
+
+superHeros.push("Superman")
+heroPower.push(2)
+
+
+type User = {
+    name: string
+    isActive: boolean
+}
+
+const allUsers: User[] =[]
+
+allUsers.push({name: "Joe", isActive: false})
+
+
+export { }
